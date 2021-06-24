39aa67232b8742a0da946184d6511f5cfec5138a
ksm <kasun@ceylonlinux.lk>
2021-06-24 20:36:32 +0530

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,11 @@ class Counter extends Component {
        
     };
     
+    renderTags(){
+        if(this.state.tags.length===0) return <p>There are no tags!</p>;
+
+        return <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>;
+    }
     
     render() { 
         
@@ -16,9 +21,8 @@ class Counter extends Component {
        
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-                </ul>
+                {this.state.tags.length===0 && "Please create a new tag!"}
+                {this.renderTags()}
             </React.Fragment>
         );
     }
