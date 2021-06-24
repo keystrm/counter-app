e85233cbe752a52efdd37c8c0104db9d986ab916
ksm <kasun@ceylonlinux.lk>
2021-06-24 21:10:43 +0530

    Binding Event Handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,19 @@ class Counter extends Component {
         tags: ['tag1','tag2','tag3']
        
     };
+
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
     
     renderTags(){
         if(this.state.tags.length===0) return <p>There are no tags!</p>;
 
         return <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>;
     }
-    handleIncrement(){
-        console.log('Increment Clicked');
+    handleIncrement=()=>{
+        console.log('Increment Clicked',this);
     }
     
     render() { 
@@ -25,7 +30,7 @@ class Counter extends Component {
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement}
                  className="btn btn-secondary btn-sm">Increment</button>
-                 
+
                 {this.state.tags.length===0 && "Please create a new tag!"}
                 {this.renderTags()}
             </React.Fragment>
