df39b07856064b446c237a7ff607d05e6ca441e9
ksm <kasun@ceylonlinux.lk>
2021-06-24 22:36:36 +0530

    Passing Event Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,11 +16,17 @@ class Counter extends Component {
         if(this.state.tags.length===0) return <p>There are no tags!</p>;
 
         return <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>;
-    }
-    handleIncrement=()=>{
-        this.setState({ count: this.state.count +1})
+    };
+
+    handleIncrement= (product) =>{
+        console.log(product);
+        this.setState({ count: this.state.count +1});
         
-    }
+    };
+
+    doHandleIncrement = () =>{
+        this.handleIncrement({id:1});
+    };
     
     render() { 
         
@@ -29,7 +35,8 @@ class Counter extends Component {
             <React.Fragment>
        
                 <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement}
+
+                <button onClick={()=> this.handleIncrement({id:1})}
                  className="btn btn-secondary btn-sm">Increment</button>
 
                 {this.state.tags.length===0 && "Please create a new tag!"}
