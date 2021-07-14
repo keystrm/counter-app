516e6662158a2de2686ee880167b28e674841802
ksm <kasun@ceylonlinux.lk>
2021-07-14 23:10:02 +0530

    sfc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    
-    render() { 
+// stateless functional components
+
+const NavBar = (props) => {
         return ( <nav className= "navbar navbar-light bg-light">
-            <a href="#" className="navbar-brand">
-                NavBar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-            </a>
-        </nav>
-        );
-    }
-}
+        <a href="#" className="navbar-brand">
+            NavBar <span className="badge badge-pill badge-secondary">
+                {props.totalCounters}</span>
+        </a>
+    </nav>
+    );  
+};
  
 export default NavBar;
+
